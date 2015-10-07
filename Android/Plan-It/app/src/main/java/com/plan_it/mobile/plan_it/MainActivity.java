b3965789c684cbf15f9153710cf5b58c0743a56e
KristianCastaneda <kcastan1@my.centennialcollege.ca>
2015-10-07 16:37:22 -0400

    Update MainActivity.java
    Fixed typo

--- Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/MainActivity.java
+++ Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/MainActivity.java
@@ -1,12 +1,11 @@
 package com.plan_it.mobile.plan_it;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
