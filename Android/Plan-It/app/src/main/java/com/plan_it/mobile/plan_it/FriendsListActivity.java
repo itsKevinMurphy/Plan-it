1c8f074b6c111653c985a84c854f97c976507533
KristianCastaneda <kcastan1@my.centennialcollege.ca>
2015-10-13 18:12:54 -0400

    Fixed Commit

--- Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/FriendsListActivity.java
+++ Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/FriendsListActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ViewEventActvity extends AppCompatActivity {
+public class FriendsListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_event_actvity);
+        setContentView(R.layout.activity_friends_list);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view_event_actvity, menu);
+        getMenuInflater().inflate(R.menu.menu_friends_list, menu);
         return true;
     }
 
