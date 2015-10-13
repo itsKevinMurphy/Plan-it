3a3988b3b0cfa22269ca1421abe29f4388426def
KristianCastaneda <kcastan1@my.centennialcollege.ca>
2015-10-13 17:00:41 -0400

    View Event UI
    Completed View Event UI for Attendees/Invitees

--- Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/ViewEventActvity.java
+++ Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/ViewEventActvity.java
@@ -4,15 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ViewEventActvity extends AppCompatActivity {
 
+    ArrayList<String> attendeeNameList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_event_actvity);
-    }
 
+        //get Attendee List and display on view event page
+        ListView attendeeList = (ListView)findViewById(R.id.lstAttendees);
+        attendeeNameList = new ArrayList<String>();
+        getAttendeeNames();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,attendeeNameList);
+        attendeeList.setAdapter(arrayAdapter);
+
+
+    }
+    void getAttendeeNames(){
+        attendeeNameList.add("Kristian");
+        attendeeNameList.add("Joanne");
+        attendeeNameList.add("Kevin");
+        attendeeNameList.add("Amina");
+        attendeeNameList.add("Luke");
+        attendeeNameList.add("Mo");
+        attendeeNameList.add("Kamran");
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
