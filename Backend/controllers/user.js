193889488df59104f0b74a042f0675512c3d6674
Seitus <seitus@live.com>
2015-10-18 19:10:39 -0400

    Create new user complete, bcrypt added
    Added ability to create and new user and encrypt user password using
    bcrypt

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -3,21 +3,61 @@ var database = require('../database');
 //Load the user model Schema
 var userSchema = require('../models/user');
 
+
 var user = module.exports;
 
+//Create a new user
 user.createUser = function(req, res, next ){
-  var userName = req.body.friendlyName;
 
-  console.log(userName);
-  console.log("Hello");
+  //Create new user object
+  var userObj = new database.userModel({
+    "friendlyName": req.body.friendlyName,
+    "email": req.body.email,
+    "hashPassword": req.body.hashPassword,
+  });
+
+  //Find an existing email address, due to unique constraint on field
+  database.userModel.findOne({"email": userObj.email}, function(err, data){
+    //If no errors found
+    if(!err)
+    {
+      //If email already exists
+      if(data)
+      {
+      console.log("Email already registered.")
+      res.status(409).send('Duplicate email address found.')
+      }
+      else
+      {
+        //Save the new user to database
+        userObj.save(function(err) {
+          if (err)
+            console.log(err);
+          else
+            res.sendStatus(201);
+        });
+      }
+    }
+    else {
+      res.sendStatus(500);
+    }
+  });
+
 };
+
 
 user.findUserByID = function(req, res, next)
 {
-  
+
 };
 
 user.updateUser = function(req, res, next)
 {
 
 };
+
+//Add a friend to an existing user
+user.addNewFriend = function(req, res, next)
+{
+
+}
