cc64589e53e5da5b8dd357bed789b5fc7955685f
Kevin Murphy <itsKevinMurphy@gmail.com>
2015-12-02 23:20:13 -0500

    Small bug fix

--- Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/FriendsListAdapter.java
+++ Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/FriendsListAdapter.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
