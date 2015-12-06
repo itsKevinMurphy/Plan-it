b0f651cfcc896138f967d90ea9d52c246c088276
Seitus <seitus@live.com>
2015-12-06 14:47:44 -0500

    Fixed add a friend
    Fixed feature to store current User ID, and add new friends

--- Frontend/views/js/services/services.js
+++ Frontend/views/js/services/services.js
@@ -70,7 +70,15 @@ angular.module('services', ['ngCookies'])
     this.logoutUser = function()
     {
       $cookies.token = "";
+      $cookies.userID = "";
     }
-
+    this.setUser = function(value)
+    {
+      $cookies.userID = value;
+    }
+    this.getUser = function()
+    {
+      return $cookies.userID;
+    }
 }
 ]);
