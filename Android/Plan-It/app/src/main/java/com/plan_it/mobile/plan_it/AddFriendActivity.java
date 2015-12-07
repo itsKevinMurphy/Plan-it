e873c4e873447fb43595f269be53cd0adc26a25a
Kevin Murphy <itsKevinMurphy@gmail.com>
2015-12-06 19:44:18 -0500

    fixes

--- Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/AddFriendActivity.java
+++ Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/AddFriendActivity.java
@@ -4,49 +4,97 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
 public class AddFriendActivity extends AppCompatActivity {
+    int friendId;
+    TextView searchResult;
+    EditText friendName;
+    String friendNameString;
+    Button addFriend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+        searchResult = (TextView)findViewById(R.id.txt_search_for_friend_result);
     }
 
-    public void SearchForFriend()
+    public void SearchForFriend(View v)
     {
-        EditText friendName = (EditText)findViewById(R.id.search_for_friend_input);
-        final String friendNameString = friendName.getText().toString();
+        searchResult.setText("");
+        addFriend = (Button)findViewById(R.id.btn_add_friend);
+        friendName = (EditText)findViewById(R.id.search_for_friend_input);
+        addFriend.setVisibility(View.INVISIBLE);
+
+        friendNameString = friendName.getText().toString();
 
         Toast.makeText(getApplicationContext(),"Searching for: " + friendNameString, Toast.LENGTH_LONG).show();
 
-        RestClient.get("user",null, LoginActivity.token, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray eventsList)
-            {
+        if(friendNameString!=null && friendNameString!="") {
+            RestClient.get("/search/" + friendNameString + "/friend", null, LoginActivity.token, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 
-            }
+                    try {
+                        JSONObject friend = response;
 
-            @Override
-            public void onFailure(int statusCode, Header[] header,Throwable throwable, JSONObject response)
-            {
-                Toast.makeText(getApplicationContext(), "Could Not Find User: " + friendNameString, Toast.LENGTH_LONG).show();
-            }
+                        searchResult.setText("User Name: " + friend.getString("friendlyName") +
+                                "\nEmail: " + friend.getString("email") +
+                                "\nFirst Name:" + friend.getString("firstName") +
+                                "\nLast Name:" + friend.getString("lastName"));
+                        addFriend.setVisibility(View.VISIBLE);
+                        friendId = friend.getInt("UserID");
 
-        });
+                        Toast.makeText(getApplicationContext(), "Success, " + friendNameString + " has been found", Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] header, Throwable throwable, JSONObject response) {
+                    Toast.makeText(getApplicationContext(), "Could Not Find User: " + friendNameString, Toast.LENGTH_LONG).show();
+                    addFriend.setVisibility(View.INVISIBLE);
+                    searchResult.setText("No User Found");
+                }
+            });
+        }
+        else if(friendNameString == null || friendNameString =="")
+        {
+            searchResult.setText("Please Enter a Valid Email or User Name");
+        }
+        searchResult.setVisibility(View.VISIBLE);
     }
 
     public  void AddFriend()
     {
+        RequestParams jdata = new RequestParams();
+        jdata.put("userID", friendId);
+        jdata.put("id", "to be done");
 
+        RestClient.post("/user/" + friendId + "/friend",  jdata, LoginActivity.token, new JsonHttpResponseHandler()
+        {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Toast.makeText(getApplicationContext(),"Success, " + response + " has been found", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] header,Throwable throwable, JSONObject response)
+            {
+
+            }
+        });
     }
 
     @Override
