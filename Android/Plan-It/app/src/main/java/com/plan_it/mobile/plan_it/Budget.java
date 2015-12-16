8cd72c1d577f0b7783cee35da116640626d0352c
KristianCastaneda <kcastan1@my.centennialcollege.ca>
2015-12-16 02:27:44 -0500

    budget isPaying update implemented properly

--- Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/Budget.java
+++ Android/Plan-It/app/src/main/java/com/plan_it/mobile/plan_it/Budget.java
@@ -4,15 +4,15 @@ package com.plan_it.mobile.plan_it;
  * Created by Kristian on 13/12/2015.
  */
 public class Budget {
-    //int userId;
+    int userId;
     String userName;
     double dividedTotal;
     double sumActCost;
     double toPay;
     boolean isPaying;
 
-    Budget(/*int userId,*/String userName,double sumActCost, double dividedTotal,double toPay, boolean isPaying){
-        //this.userId = userId;
+    Budget(int userId,String userName,double sumActCost, double dividedTotal,double toPay, boolean isPaying){
+        this.userId = userId;
         this.userName = userName;
         this.sumActCost = sumActCost;
         this.dividedTotal = dividedTotal;
